a5e0aa16133909bcdea509ca31496f041609ed5c
yejikk <dpwl7484@gmail.com>
2019-06-04 14:01:32 +0900

    폴더정리

--- Nodejs/실습코드/nodejs/readdir.js
+++
@@ -1,9 +0,0 @@
-// node가 실행되는 폴더를 기준으로 경로를 설정한다.
-const testFolder = './data';  
-// File System 모듈을 불러온다.
-const fs = require('fs');
-
-// function의 파라미터의 이름은 마음대로 해도 좋으나 순서는 반드시 지키자.
-fs.readdir(testFolder, function(error, fileList) {
-    console.log(fileList);
-});
